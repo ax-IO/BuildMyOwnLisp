8bae6ce76c841ce4b3b607b0053f0173c452ff3b
Gauthier Germain <g.germain98@gmail.com>
2018-01-11 23:42:51 +0100

    head + tail : OK

--- lispy.c
+++ lispy.c
@@ -36,6 +36,8 @@
   lval* builtin_op (lval* a, char* op);
 
   lval* lval_qexpr (void);
+
+
 int main(int argc, char** argv) {
 /* Create Some Parsers */
   mpc_parser_t* Number   = mpc_new("number");
@@ -49,7 +51,9 @@ int main(int argc, char** argv) {
 mpca_lang(MPCA_LANG_DEFAULT,
   "                                          \
     number : /-?[0-9]+/ ;                    \
-    symbol : '+' | '-' | '*' | '/' ;         \
+    symbol : \"list\" | \"head\" | \"tail\"  \
+           | \"join\" | \"eval\"             \
+           |'+' | '-' | '*' | '/' ;          \
     sexpr  : '(' <expr>* ')' ;               \
     qexpr  : '{' <expr>* '}' ;               \
     expr   : <number> | <symbol> |           \
@@ -283,4 +287,50 @@ lval* builtin_op (lval* a, char* op){
   }
   lval_del(a);
   return x;
+}
+
+lval* builtin_head (lval* a){
+  /* Check Error Conditions*/
+  if (a->count != 1){
+    lval_del(a);
+    return lval_err("Function 'head' passed too many arguments!");
+  }
+  if (a->cell[0]->type != LVAL_QEXPR){
+    lval_del(a);
+    return lval_err("Function 'head' passed incorrect types!");
+  }
+  if (a->cell[0]->count == 0){
+    lval_del(a);
+    return lval_err("Function 'head' passed '{}'!");
+  }
+  /* Otherwise take first argument */
+  lval* v = lval_take(a, 0);
+
+  /* Delete all elements that are not head and return  */
+  while (v->count > 1){
+    lval_del(lval_pop(v, 1));
+  }
+  return v;
+}
+
+lval* builtin_tail (lval* a){
+  /* Check Error Conditions*/
+  if (a->count != 1){
+    lval_del(a);
+    return lval_err("Function 'tail' passed too many arguments!");
+  }
+  if (a->cell[0]->type != LVAL_QEXPR){
+    lval_del(a);
+    return lval_err("Function 'tail' passed incorrect types!");
+  }
+  if (a->cell[0]->count == 0){
+    lval_del(a);
+    return lval_err("Function 'tail' passed '{}' !");
+  }
+    /* Otherwise take first argument */
+  lval* v = lval_take(a, 0);
+
+  /* Delete all elements that are not head and return  */
+  lval_del(lval_pop(v, 0));
+  return v;
 }
